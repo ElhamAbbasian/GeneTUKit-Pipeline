7216e65510b433a3a2903473068e9154f8e3918c
KerstenDoering <kersten.doering@pharmazie.uni-freiburg.de>
2015-09-21 17:19:52 +0200

    Major update

--- filter_idmapping.py
+++ filter_idmapping.py
@@ -3,67 +3,70 @@
 
 """
     Copyright (c) 2015, Elham Abbasian <e_abbasian@yahoo.com>, Kersten Doering <kersten.doering@gmail.com>
-"""
 
-#Elham 22.07.2014
-#This script is for filtering idmapping.dat file , in a way that we are searching in ID-Type column (second column) , for lines with the "Gene-ID" value , and save
-# the first and third coulmn which are protein-id and Gene-id respectively in a .csv file (filteredidmapping.csv) .
-#Input lines are in 3 columns like this : protein_ID    ID-Type    Gene-ID
-#output format is like , searching in ID-Type coloumn and for the lines with "GeneID" value , write a line in output like : protein_ID    Gene-ID
+    This script filters pairs of UniProt IDs and gene IDs from the file idmapping.dat. The first and the third coulmn, which are UniProt Id and gene Id (if the second column equals "GeneID"), are saved in the file filtered_idmapping.csv. A test case using only gene IDs from the GeneTUKit output file pmid_geneid_syn.csv can be run with "-t".
+"""
 
 #optparse - Parser for command line options
 from optparse import OptionParser
 
+if __name__=="__main__":
+    parser= OptionParser()
+    parser.add_option("-t", "--test_case", dest="t", action="store_true", default=False, help="Run this script with a given number of PubMed IDs in pmid_geneid_syn.csv (default: False).")    
+    parser.add_option("-i", "--input", dest="i", help='name of the input file with gene IDs, UniProt IDs, and other information',default="idmapping.dat")
+    parser.add_option("-r", "--result", dest="r", help='name of the other input file with the results from GeneTUKit',default="pmid_geneid_syn.csv")
+    parser.add_option("-o", "--output", dest="o", help='name of the output file containing only UniProt IDs and gene IDs',default="filtered_idmapping.csv")
 
-parser= OptionParser()
-parser.add_option("-i", "--input", dest="i", help='true or false. This option specifies wether the user uses this pipeline as a test case or not',default="true")
+    (options,args)=parser.parse_args()
 
-(options,args)=parser.parse_args()
+    #save parameters in an extra variable
+    test_case = options.t
+    input_file = options.i
+    output_file = options.o
+    results_file = options.r
 
-#save parameters in an extra variable
-test_case = options.i
+    # open files
+    infile = open (input_file,"r")
+    outfile = open(output_file,"w")
+    results=open(results_file,"r")
 
-infile = open ("idmapping.dat","r")
-outfile = open("filtered_idmapping.csv","w")
-pgs_file=open("pmid_geneid_syn.csv","r")
+    # for each line in pmid_geneid_syn.csv, get the gene ID and save it in a list
+    gene_id_list=[]
+    for line in results:
+        temp = line.strip().split("\t")
+        gene_id_list.append(temp[1])
 
-#for every line in "pmid_geneid_syn.csv" file, fetch the gene-id and save in a list.
-gene_id_list=[]
-for line in pgs_file:
-	temp = line.strip().split("\t")
-	gene_id_list.append(temp[1])
+    # test case with a small number of gene IDs from pmid_geneid_syn.csv
+    if test_case:
+        print gene_id_list
 
-print gene_id_list
-
-
-#read every line in file - line is a string
-for line in infile:
-    #strip deletes leading plus ending spaces, etc.
-    #split generates a list out of a string and deletes "delimiter"
-    temp = line.strip().split("\t")
-    
-    #debug:
-    #Check for second column , if it's value is "GeneID" , then write the required columns in output file
-    if test_case == "true" :
-        idtype="GeneID"
-        try:
-            if temp[1] == idtype:
-                if temp[2] in gene_id_list:
+    #read every line in file - line is a string
+    for line in infile:
+        # strip() deletes leading plus ending spaces, etc.
+        # split(delimiter) generates a list out of a string and deletes the "delimiter" (here: tab)
+        temp = line.strip().split("\t")
+        # Check the second column - if its value equals "GeneID", the required UniProt ID and gene ID is stored in the output file
+        # debug:
+        # if test_case is True
+        if test_case:
+            idtype="GeneID"
+            try:
+                if temp[1] == idtype:
+                    if temp[2] in gene_id_list:
+                        outfile.write(temp[0]+"\t"+temp[2]+"\n")
+            # debug:
+            except:
+                print line
+        else:
+            idtype="GeneID"
+            try:
+                if temp[1] == idtype:
                     outfile.write(temp[0]+"\t"+temp[2]+"\n")
-        except:
-            print line
-    else :
-        idtype="GeneID"
-        try:
-            if temp[1] == idtype:
-                outfile.write(temp[0]+"\t"+temp[2]+"\n")
+            # debug:
+            except:
+                print line
 
-        except:
-            print line
-
-
-
-#close file pointers
-outfile.close()
-infile.close()
+    # close files
+    outfile.close()
+    infile.close()
 
