62971ad57a376004247009241546d47b5a39dc8e
KerstenDoering <kersten.doering@pharmazie.uni-freiburg.de>
2015-10-08 19:45:03 +0200

    Updated

--- map_to_dict.py
+++ map_to_dict.py
@@ -3,32 +3,28 @@
 
 """
     Copyright (c) 2015, Elham Abbasian <e_abbasian@yahoo.com>, Kersten Doering <kersten.doering@gmail.com>
+
+    This script reads the file filtered_idmapping.csv (UniProt ID \t gene ID) and stores its data in a dictionary with the gene ID as the key and the UniProt ID(s) as the value in a list.
 """
 
-# open the filteredidmapping.csv file , which each line should be like this : Uniprot-ID    Gene-ID
-#and save it's data into a Dictionary datastructure . in a way of 'geneid:unipotids'
-
+# read input file
 input_file = open ("filtered_idmapping.csv","r")
-
-#define an empty dictionary to save our pairs of key:values
+#dictionary initialisation
 idmapping={}
-
 #read each line in file
 for line in input_file:
     temp = line.strip().split("\t")
-
+    # UniProt ID \t gene ID
     geneid = temp[1]
     uniprotid = temp[0]
-
+    # if gene ID is not yet stored in the dictionary
     if not (geneid in idmapping):
-        #initialize the Dictionary with new Gene-ID and its value
+        #key: gene ID, value: list of UniProt IDs (or list with one element)
         idmapping[geneid] = [uniprotid]
-
-    elif (geneid in idmapping) and (not (uniprotid in idmapping[geneid]) ) :
-        #append the new uniprotid to existing geneid key
+    # else: append UniProt ID to the list if it is not yet inside
+    elif (geneid in idmapping) and (not (uniprotid in idmapping[geneid])):
         idmapping[geneid].append(uniprotid)
-
-#close the pointer 
+#close file
 input_file.close()
 
 
